93ce290e11bbc2987605ad7cc5beb04d5fa00d64
sheikhlimon <sheikhlimon404@gmail.com>
2025-11-13 17:14:31 +0600

    Fix React import error in App.tsx
    - Added React import to support React.memo usage
    - This resolves the 'React is not defined' error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect, useRef } from "react";
+import React, { useState, useCallback, useEffect, useRef } from "react";
 import { DesktopView } from "./components/DesktopView";
 import { MobileView } from "./components/MobileView";
 
